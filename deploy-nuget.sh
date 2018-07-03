95785cfcff1379193152f23b877803714e66fac3
Ilya Breev <ilyabreev@gmail.com>
2018-07-03 09:18:35 +0300

    removed verbosity from deploy script

--- deploy-nuget.sh
+++ deploy-nuget.sh
@@ -1,5 +1,5 @@
 ApiKey=$1
 Source=$2
 
-dotnet pack -c Release --no-restore --no-build ./src/ClickHouse.Net/ClickHouse.Net.csproj -o ../../ -v detailed
-dotnet nuget push ./ClickHouse.Net.*.nupkg -v detailed -k $ApiKey -s $Source
+dotnet pack -c Release --no-restore --no-build -o ../../ ./src/ClickHouse.Net/ClickHouse.Net.csproj 
+dotnet nuget push -k $ApiKey -s $Source ./ClickHouse.Net.*.nupkg 
